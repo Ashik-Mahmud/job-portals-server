2fed2902a5cab94d22fdd4c9d1cfed71878e62f6
ashikmahmud <ashikmamud187@gmail.com>
2022-10-18 00:04:58 +0600

    init server and redirect to the index.html

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require('cors');
+
+
+// Serve static files from the React app & CORS & Apply middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(express.static(path.resolve('uploads')))
+
+
+// Init very First Route for App
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve('views/index.html'));
+});
+
+module.exports = app;
