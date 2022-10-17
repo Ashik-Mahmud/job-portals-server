46eb983dcf70300175784dc0e4d73a772c8bba78
ashikmahmud <ashikmamud187@gmail.com>
2022-10-18 00:40:12 +0600

    create suer model and instance for creating auth

---
+++ models/Jobs.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
+const jobsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+const Job = mongoose.model('Jobs', jobsSchema);
+module.exports = Job;
---
+++ models/User.model.js
@@ -0,0 +1,140 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 7,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error('Password cannot contain "password"');
+        }
+      },
+    },
+    role: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      enum: ["candidate", "hr", "admin"],
+    },
+    address: {
+      type: String,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      trim: true,
+    },
+    socialLinks: [
+      {
+        facebook: {
+          type: String,
+          trim: true,
+        },
+        twitter: {
+          type: String,
+          trim: true,
+        },
+        linkedin: {
+          type: String,
+          trim: true,
+        },
+      },
+    ],
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    status: {
+      type: String,
+      default: "active",
+      enum: ["active", "inactive"],
+    },
+
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.virtual("jobs", {
+  ref: "Job",
+  localField: "_id",
+  foreignField: "owner",
+});
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign(
+    { _id: user._id.toString() },
+    process.env.TOKEN_SECRET
+  );
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+  return user;
+};
+
+// Hash the plain text password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
