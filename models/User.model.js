3e06688d2a301024487de915fe43299b7c071736
ashikmahmud <ashikmamud187@gmail.com>
2022-10-18 00:46:07 +0600

    added dbConnection js file in the utils folder and done to code

--- models/User.model.js
+++ models/User.model.js
@@ -90,6 +90,7 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+
 userSchema.virtual("jobs", {
   ref: "Job",
   localField: "_id",
@@ -131,10 +132,19 @@ userSchema.statics.findByCredentials = async (email, password) => {
 userSchema.pre("save", async function (next) {
   const user = this;
   if (user.isModified("password")) {
-    user.password = await bcrypt.hash(user.password, 8);
+    user.password = await bcrypt.hash(user.password, 10);
   }
   next();
 });
+
+// Compared Password 
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
---
+++ utils/dbConnection.js
@@ -0,0 +1,10 @@
+const dbConnection = () => {
+  const db = mongoose.connect(process.env.DB_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+  return db;
+};
+module.exports = dbConnection;
