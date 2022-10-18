c86355eb43d078f0a9572b9c5b51381f9411be95
ashikmahmud <ashikmamud187@gmail.com>
2022-10-19 00:30:18 +0600

    create endpoints to get all the candidates with applied jobs

--- services/admin.service.js
+++ services/admin.service.js
@@ -3,7 +3,7 @@ const User = require("../models/User.model");
 /* Get All the candidates with Applied Job */
 exports.getAllCandidatesService = async () => {
   try {
-    const candidates = await User.find({role: "candidate"}).populate("appliedJobs");
+    const candidates = await User.find({role: "candidate"}).populate("appliedJobs", "-appliedCandidates -hiringManager -createdAt -updatedAt -__v");
     return {
       success: true,
       message: "Candidates with applied jobs fetched successfully.",
